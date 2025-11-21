3d5e4f92a92af625e220d8d38ef40d2e179e32ee
Lucas Machado <lu.machado.m397@gmail.com>
2025-11-21 12:09:22 -0300

    feat(quickSort) cria a quickSort

--- QuickSort/QuickSort.java
+++ QuickSort/QuickSort.java
@@ -1,4 +1,49 @@
 package QuickSort;
+import java.util.ArrayList;
+
+import ReservaType.Reserva;
+
 public class QuickSort{
-    
+    ArrayList<Reserva> lista;
+
+    public QuickSort(ArrayList<Reserva> data){
+        this.lista = data;
+    }
+
+    public ArrayList<Reserva> ordena(){
+        ordena(0, this.lista.size() -1);
+        return this.lista;
+    }
+
+    public void ordena(int esq, int dir){
+
+        int i = esq, j = dir;
+        String pivo; 
+        pivo = this.lista.get((i+j)/2).getCodReserva();
+
+        do{
+            while(this.lista.get(i).getCodReserva().compareTo(pivo) < 0){
+                i++;
+            }
+            while(this.lista.get(j).getCodReserva().compareTo(pivo) > 0){
+                j++;
+            }
+
+            if(i <= j){
+                Reserva temp = this.lista.get(i);
+                this.lista.set(i, this.lista.get(j));
+                this.lista.set(j, temp);
+                j++;
+                i--; 
+            }
+        }while(i <= j);
+        
+        if(esq < j){
+            ordena(esq, j);
+        }
+
+        if(dir > i){
+            ordena(esq, j);
+        }
+    }
 }
